d5dd5cb04bc118ff756a838307f6854bc82ff132
upbringingeipfm <upbringing.eipfm@outlook.com>
2020-04-17 22:26:23 +0530

    first commit

--- app.js
+++ app.js
@@ -43,22 +43,21 @@ function showScores() {
 
 // create questions
 var questions = [
-    new Question("Entire parts of the following drugs are effective, except", [" Ergot", "Belladonna","Ephedra", "Clove"], "Clove"),
+    new Question(" The role of borax in cold creams is ", ["anti-microbial agent", " to provide fine particles to polish skin"," in-situ emulsifier", " antioxidant"], "anti-microbial agent"),
 
-    new Question("Select the drug, which is not belonging to glycoside class?", ["Digitalis", "senna","Nux vomica", "Cascara"], "Nux vomica"),
-
-
-    new Question("Drug is not under the class of organized drug :", ["Leaves", "flowers","fruits", "Gums"], "Gums"),
+    new Question(" Choose the right combination: ", [" Quinine', antimalarial, isoquinoline alkaloid ", " Reserpine, -antihypertensive, indole alkaloid"," Quantitative microscopy, stomatal number, myrrh", "Palmitic acid, salicylic acid , fatty acids"], " Reserpine, -antihypertensive, indole alkaloid"),
 
 
-    new Question("Drug is not under the seed class ", ["Nux vomica", "Digitalis","stropanthus", "Ispgol"], "Digitalis"),
+    new Question("Triterpenoids are active constituents of", ["Jaborandi", " Rhubarb ","Stramonium", " Brahmi"], " Brahmi"),
 
-  new Question("Drug not belonging to volatile oil class:", ["Peppermint", "Clove","Castor oil", "Garlic"], "Castor oil"),
-    new Question("______ is not used as cardiotonics", ["Digitalis", "Cincona","Squill", "Stropanthus"], "Cincona"),
-      new Question("Drug is used as antimalerial :", ["Ashwagandha", "Tulsi","Ginseng", "Artemesia"], "Artemesia"),
-        new Question("Tannins give colour with iron compound :", ["Pale yellow", "Blue black","Light pink", "Orange"], "Light pink"),
-          new Question("Flower bud of drug showing medicinal importance:", ["Saffrom", "Clove","Fig", "Caraway"], "Clove"),
-            new Question("Alkaloids are ________ type of substances", ["Acid", "Neutral","Chemical", "Basic nitrogenous"], "Basic nitrogenous	"),
+    new Question(" Alkaloids are NOT precipitated by", ["Mayer's reagent", "Dragendroff Reagent ","Picric acid", "Millon's reagent"], "Millon's reagent"),
+
+  new Question(" Anisocytic stomata are present in", ["Senna", "Digitalis","Belladonn", "Coca"], "Belladonn"),
+    new Question("Tropane alkaloids are NOT present in ", ["Datura stramonium", "Erythroxylum coca ","Duboisia myoporoides ", "Lobelia inflata"], "Lobelia inflata"),
+      new Question(" Guggul lipids are obtained from ", ["Commiphora molmol", "Boswellia serrata","Commiphora wightii ", " Commiphora abyssinica"], "Commiphora wightii"),
+        new Question("An example of N-glycoside is", ["Adenosine", "Sinigrin","Rhein-8-glucoside", "Aloin"], "Adenosine"),
+          new Question("One mg of Lycopodium spores used in quantitative microscopy contains an average of ", ["94,000 spores", "92,000 spores","90,000 spores", "91,000 spores"], "94,000 spores"),
+            new Question(" Select the correc: combination of drugs for the treatment of patients suffering from Hepatitis C", ["Interferon with Ribavirin", "Interferon with Zidovudine "," Interferon with Stavudine", "Interferon with"], "Interferon with Zidovudine"),
 ];
 
 
